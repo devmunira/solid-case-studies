e60f1f7543bf52dc49a909ec8fd8ff1e751b6098
devmunira <fswfofficial@gmail.com>
2025-03-11 10:51:57 +1300

    different payment process verification system design using SOLID

---
+++ src/Case-Study/8/index.ts
@@ -0,0 +1,48 @@
+interface IPaymentProcess {
+  payment(): void;
+}
+
+interface ICreditCardVerification {
+  verifyCVV(): void;
+  verifyExpiryDate(): void;
+}
+
+interface IBankVerification {
+  verifyAccountNumber(): void;
+}
+
+interface IDigitalWalletVerification {
+  verifyEmailOrPhone(): void;
+}
+
+class CreditCardPayment implements ICreditCardVerification, IPaymentProcess {
+  payment(): void {
+    console.log("Payment Process");
+  }
+  verifyCVV(): void {
+    console.log("Verification for Credit Card");
+  }
+  verifyExpiryDate(): void {
+    console.log("Verification for Credit Card");
+  }
+}
+
+class BankPayment implements IBankVerification, IPaymentProcess {
+  payment(): void {
+    console.log("Payment Process");
+  }
+  verifyAccountNumber(): void {
+    console.log("Payment Verify");
+  }
+}
+
+class DigitalPayment implements IDigitalWalletVerification, IPaymentProcess {
+  payment(): void {
+    console.log("Payment Process");
+  }
+  verifyEmailOrPhone(): void {
+    console.log("Payment Verify");
+  }
+}
+
+const payment = new DigitalPayment().verifyEmailOrPhone();
